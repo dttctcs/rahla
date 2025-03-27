523f608d024c64a35c1d42077ab36311bf860823
Matthias Leinweber <m.leinweber@datatactics.de>
2025-03-27 16:41:12 +0100

    more dind

--- rahla/src/main/java/rahla/extensions/GroovyBeanFactoryImpl.java
+++ rahla/src/main/java/rahla/extensions/GroovyBeanFactoryImpl.java
@@ -12,6 +12,9 @@ import rahla.api.GroovyBeanFactory;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 @Component
 @Log4j2
@@ -38,17 +41,17 @@ public class GroovyBeanFactoryImpl implements GroovyBeanFactory {
 
 
   @Override
-  public Object createBean(String resource) {
-
-
+  public Object createBean(String urlSpec) {
     try {
       Class clazz;
-      if (resource.startsWith(RESOURCE_FILE)) {
-        String fileName = resource.substring(RESOURCE_FILE.length());
+      if (urlSpec.startsWith(RESOURCE_FILE)) {
+        log.warn("resource:file: is deprecated and will be removed in a future release! Use createBean(String urlSpec) instead");
+        String fileName = urlSpec.substring(RESOURCE_FILE.length());
         File file = new File(fileName);
         clazz = groovyClassLoader.parseClass(file);
-      } else if (resource.startsWith(RESOURCE_DEPLOY)) {
-        String fileName = resource.substring(RESOURCE_DEPLOY.length());
+      } else if (urlSpec.startsWith(RESOURCE_DEPLOY)) {
+        log.warn("resource:deploy: is deprecated and will be removed in a future release! Use createBean(String urlSpec) instead");
+        String fileName = urlSpec.substring(RESOURCE_DEPLOY.length());
         if(fileName.startsWith("/")){
           fileName = fileName.substring(1);
         }
@@ -56,11 +59,14 @@ public class GroovyBeanFactoryImpl implements GroovyBeanFactory {
         File file = new File(fileName);
         clazz = groovyClassLoader.parseClass(file);
       } else {
-        clazz = groovyClassLoader.parseClass(resource);
+        URL url = new URL(urlSpec);
+        InputStream inputStream = url.openStream();
+        String clazzString = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+        inputStream.close();
+        clazz = groovyClassLoader.parseClass(clazzString);
       }
-
       Object bean = newBean(clazz);
-      log.info("bean_created={}", resource);
+      log.info("Bean of class {} created from: {}", bean.getClass().getName(), urlSpec);
       return bean;
     } catch (Exception e) {
       throw new RuntimeException(e);
